52f26ebcc6e63325817e54bd583dbf7692cb299f
hoffnik <hoffnik.dev@gmail.com>
2022-07-08 23:17:47 -0700

    added HTMl to codebase, learned .map() and .filter() array methods

--- app.js
+++ app.js
@@ -2,6 +2,53 @@
 /* the argv property of process is an array that holds exactly what was typed into the command line upon execution so that we can capture that data and use it in the app */
 
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+const mockData = {
+  name: 'Lernantino',
+  github: 'lernantino',
+  confirmAbout: true,
+  about:
+    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+  projects: [
+    {
+      name: 'Run Buddy',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['HTML', 'CSS'],
+      link: 'https://github.com/lernantino/run-buddy',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskinator',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'HTML', 'CSS'],
+      link: 'https://github.com/lernantino/taskinator',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskmaster Pro',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+      link: 'https://github.com/lernantino/taskmaster-pro',
+      feature: false,
+      confirmAddProject: true
+    },
+    {
+      name: 'Robot Gladiators',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+      languages: ['JavaScript'],
+      link: 'https://github.com/lernantino/robot-gladiators',
+      feature: false,
+      confirmAddProject: false
+    }
+  ]
+};
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -134,13 +181,20 @@ if (!portfolioData.projects) {
     });
   }
 
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+  const pageHTML = generatePage(mockData);
+
+  // promptUser()
+  // .then(promptProject)
+  // .then(portfolioData => {
+  //   const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  // });
+
 
 // ______________________________________________________________
 // const printProfileData = profileDataArr => {
@@ -166,13 +220,5 @@ if (!portfolioData.projects) {
   // const name = profileDataArgs[0];
   // const github = profileDataArgs[1];
   // or use assignment destructuring to just use one line: const [name, github] = profileDataArgs;
-// ______________________________________________________________________
 
-// const pageHTML = generatePage(name, github);
-
-//   fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
   
