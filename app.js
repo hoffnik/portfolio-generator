a6c4457ce1723c116eb6ecb19695dfc7612d6a10
hoffnik <hoffnik.dev@gmail.com>
2022-07-09 00:47:52 -0700

    added promises and refractored

--- app.js
+++ app.js
@@ -2,53 +2,12 @@
 /* the argv property of process is an array that holds exactly what was typed into the command line upon execution so that we can capture that data and use it in the app */
 
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
+// const generateSite = require('./utils/generate-site.js');
+// simplified destructured version:
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 const generatePage = require('./src/page-template.js')
-const mockData = {
-  name: 'Lernantino',
-  github: 'lernantino',
-  confirmAbout: true,
-  about:
-    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-  projects: [
-    {
-      name: 'Run Buddy',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['HTML', 'CSS'],
-      link: 'https://github.com/lernantino/run-buddy',
-      feature: true,
-      confirmAddProject: true
-    },
-    {
-      name: 'Taskinator',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['JavaScript', 'HTML', 'CSS'],
-      link: 'https://github.com/lernantino/taskinator',
-      feature: true,
-      confirmAddProject: true
-    },
-    {
-      name: 'Taskmaster Pro',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-      link: 'https://github.com/lernantino/taskmaster-pro',
-      feature: false,
-      confirmAddProject: true
-    },
-    {
-      name: 'Robot Gladiators',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-      languages: ['JavaScript'],
-      link: 'https://github.com/lernantino/robot-gladiators',
-      feature: false,
-      confirmAddProject: false
-    }
-  ]
-};
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -181,18 +140,42 @@ if (!portfolioData.projects) {
     });
   }
 
-  const pageHTML = generatePage(mockData);
+  const pageHTML = generatePage(portfolioData);
 
-  // promptUser()
-  // .then(promptProject)
-  // .then(portfolioData => {
-  //   const pageHTML = generatePage(portfolioData);
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-    fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-
-       console.log('Page created! Check out index.html in this directory to see it!');
-     });
+  // fs.writeFile('./dist/index.html', pageHTML, err => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   console.log('Page created! Check out index.html in this directory to see it!');
+  
+  //   fs.copyFile('./src/style.css', './dist/style.css', err => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log('Style sheet copied successfully!');
+  //   });
+  // });
   // });
 
 
